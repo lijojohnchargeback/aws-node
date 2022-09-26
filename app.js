bd675d20bb7ec4c4e6b5be743a3d2aacc27473f8
lijojohnchargeback <104132081+lijojohnchargeback@users.noreply.github.com>
2022-09-26 11:56:29 +0530

    lates

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
     "<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>"
   );
 });
-//dalkjfddafda
+
 app.get("/products", (req, res) => {
   res.send([
     {
@@ -20,7 +20,6 @@ app.get("/products", (req, res) => {
     },
   ]);
 });
-//daflk
 app.listen(port, () => {
   console.log(`Demo app is up and listening to port: ${port}`);
 });
